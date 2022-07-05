f17cadb5ebf0ded943ceb2f9e402f4c88ac9250f
gunjan <gunupatel1998@gmail.com>
2022-07-05 12:09:05 +0530

    springboot

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+ package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
